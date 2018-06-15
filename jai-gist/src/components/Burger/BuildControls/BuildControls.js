d3edd672693c3c6f71574e33bd837cc9209dd875
Avnita Bir <jaideep@yrals.com>
2018-06-14 20:55:26 -0400

    burger 1.1.0 build controls added

--- jai-gist/src/components/Burger/BuildControls/BuildControls.js
+++ jai-gist/src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Meat', type: 'meat'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'Cheese'},
+];
 
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        
+    {controls.map(ctrl => (<BuildControl key={ctrl.label} label={ctrl.label} />))}
     </div>
 );
 
